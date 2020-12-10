f5cd2ad88a7a1a78c09f2c84de84a5364fa678fe
Andrew Smith <andrew.smith2@nbt.nhs.uk>
2020-12-10 11:05:23 +0000

    creation of plotModule(), code formatting, adding comments

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 import csv
 import os
+import random
 
+# path to file
 path = os.path.join(
     os.sep, 
     "media", 
@@ -15,69 +17,69 @@ path = os.path.join(
     "Cardiomyopathies - including childhood onset", 
     "moduland", 
     )
-
 bridgnessCentralityFile = os.path.join(path, "bridgness_centrality.csv")
+
+# set up pandas DF
 df = pd.read_csv(bridgnessCentralityFile) 
 df['moduland_betweenness'] = df['moduland_betweenness'].apply(lambda x: float(x))
 df['moduland_centrality'] = df['moduland_centrality'].apply(lambda x: float(x))
 
-# sorting modules into sub df's
-modules = {
-    "MLCYD":df[(df['module'] == "MLCYD")],
-    "NTRK1":df[(df['module'] == "NTRK1")],
-    "HCN1":df[(df['module'] == "HCN1")],
-}
-
+# Pd series of gene names
 genes = df.name
-
-panelAppGreen = df[(df['panelAppGreen'] == 1)]
-
-# Matplotlib
-x = df.moduland_centrality
-y = df.moduland_betweenness
-
-xMLCYD = modules["MLCYD"].moduland_centrality
-yMLCYD = modules["MLCYD"].moduland_betweenness
-
-xNTRK1 = modules["NTRK1"].moduland_centrality
-yNTRK1 = modules["NTRK1"].moduland_betweenness
-
-xHCN1 = modules["HCN1"].moduland_centrality
-yHCN1 = modules["HCN1"].moduland_betweenness
-
-xPanelAppGreen = panelAppGreen.moduland_centrality
-yPanelAppGreen = panelAppGreen.moduland_betweenness
-
 
 def regression(x,y):
     a, b = np.polyfit(np.array(x), np.array(y), deg=1)
     f = lambda x: a*np.array(x) + b
     return f
 
-fMLCYD = regression(xMLCYD, yMLCYD)
-fNTRK1 = regression(xNTRK1, yNTRK1)
-fHCN1 = regression(xHCN1, yHCN1)
+def plotModule(df, moduleName, plotAxes, colour, marker="s"):
 
+    # Set variables
+    x = df.moduland_centrality
+    y = df.moduland_betweenness
+    subDf = df[(df['module'] == moduleName)]
+    xsubDf = subDf.moduland_centrality
+    ysubDf = subDf.moduland_betweenness
+    fsubDf = regression(xsubDf,ysubDf)
+
+    # Plot chart
+    plotAxes.scatter(xsubDf, ysubDf, c=colour, marker=marker, label=moduleName)
+    # Plot regression line
+    plotAxes.plot(xsubDf,fsubDf(xsubDf),lw=1.5, c=colour)
+
+    # Annotate points
+    lstGenes=subDf.name.tolist() 
+    for i, gene in enumerate(genes):
+        if gene in lstGenes:
+            ax1.annotate(gene, (x[i], y[i]))
+
+# Set-up plot
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 ax1.set_xlabel("Centrality")
 ax1.set_ylabel("Betweeness")
-# ax1.scatter(x, y, c='k', marker="s", label='All entries')
-ax1.scatter(xMLCYD, yMLCYD, c='r', marker="s", label='MLCYD')
-ax1.scatter(xNTRK1, yNTRK1, c='gold', marker="s", label='NTRK1')
-ax1.scatter(xHCN1, yHCN1, c='g', marker="s", label='HCN1')
-ax1.scatter(xPanelAppGreen, yPanelAppGreen, c='cyan', marker="x", label='panelAppGreen')
 
-ax1.plot(xMLCYD,fMLCYD(xMLCYD),lw=1.5, c="r")
-ax1.plot(xNTRK1,fNTRK1(xNTRK1),lw=1.5, c="gold")
-ax1.plot(xHCN1,fHCN1(xHCN1),lw=1.5, c="g")
+# Plot moduland clusters (modules)
+plotModule(df,"MLCYD", ax1, "cyan")
+plotModule(df,"NTRK1", ax1, "gold")
+plotModule(df,"HCN1", ax1, "r")
 
-plt.legend(loc='upper left')
-
+# sub DF for panelApp Genes (green)
+panelAppGreen = df[(df['panelAppGreen'] == 1)]
+# plot panelApp Genes (Green)
+x = df.moduland_centrality
+y = df.moduland_betweenness
+xPanelAppGreen = panelAppGreen.moduland_centrality
+yPanelAppGreen = panelAppGreen.moduland_betweenness
+ax1.scatter(xPanelAppGreen, yPanelAppGreen, c='limegreen', marker="x", label='panelAppGreen')
+ax1.plot(xPanelAppGreen,regression(xPanelAppGreen,yPanelAppGreen)(xPanelAppGreen),lw=1.5, c="limegreen")
 panelAppGreenGenes=panelAppGreen.name.tolist() 
 for i, gene in enumerate(genes):
     if gene in panelAppGreenGenes:
         ax1.annotate(gene, (x[i], y[i]))
+
+# Add legend
+plt.legend(loc='upper left')
 
 plt.show()
 
